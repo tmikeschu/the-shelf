fa57d2dc87da94639aec6acaf929997d09a0ee5a
Mike Schutte <schutte@clinc.com>
2019-03-07 18:59:41 -0500

    Add base grid and first input field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>The Shelf</h1>
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [showAddItem, setShowAddItem] = useState(false);
+  const addItem = () => {
+    setItems(items.concat({}));
+  };
+
+  return (
+    <div className="App">
+      <h1>The Shelf</h1>
+      <div className="App__Shelf">
+        {items.map(item => (
+          <div className="App__Item" key={item.upcId}>
+            {item.name}
+          </div>
+        ))}
+        {[...Array(25 - items.length)].map((_, i) => (
+          <div
+            key={i}
+            className="App__Item --empty"
+            role="button"
+            onClick={() => setShowAddItem(!showAddItem)}
+          />
+        ))}
       </div>
-    );
-  }
-}
+
+      {showAddItem && (
+        <form className="App__AddItem" onSubmit={addItem}>
+          <input type="text" value="" />
+        </form>
+      )}
+    </div>
+  );
+};
 
 export default App;
