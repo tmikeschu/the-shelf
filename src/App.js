f1312adf5dd52332b0567704beebf88c63f98887
Mike Schutte <schutte@clinc.com>
2019-03-08 11:51:12 -0500

    Clean up styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import "./App.css";
 
 /*
  * TODO:
- * Refactor repetitive array update code
  * Add pagination
- * Clean up styling
- * Use custom delete confirmation
  */
+
+const capitalize = s => s[0].toUpperCase() + s.slice(1).toLowerCase();
+
+const updateElement = xs => index => fn => [
+  ...xs.slice(0, index),
+  fn(xs[index]),
+  ...xs.slice(index + 1)
+];
 
 const App = () => {
   const [items, setItems] = useState([...Array(25)]);
@@ -23,11 +28,11 @@ const App = () => {
   const [size, setSize] = useState("");
   const [upcId, setUpcId] = useState("");
 
-  const shoeProps = {
+  const itemProps = {
     brand: { val: brand, setter: setBrand },
     style: { val: style, setter: setStyle },
     size: { val: size, setter: setSize },
-    upcId: { val: upcId, setter: setUpcId }
+    upcId: { val: upcId, setter: setUpcId, label: "UPC ID" }
   };
 
   const addItem = e => {
@@ -38,11 +43,7 @@ const App = () => {
       size,
       upcId
     };
-    const newItems = [
-      ...items.slice(0, selectedIndex),
-      item,
-      ...items.slice(selectedIndex + 1)
-    ];
+    const newItems = updateElement(items)(selectedIndex)(() => item);
     setItems(newItems);
     [setBrand, setStyle, setSize, setUpcId].map(f => f(""));
     setShowAddItem(false);
@@ -61,22 +62,34 @@ const App = () => {
     setSelectedIndex(slotIndex);
   };
 
-  const handleEdit = i => () => {
+  const handleEdit = i => e => {
+    e.stopPropagation();
     setSelectedIndex(i);
-    Object.entries(shoeProps).forEach(([prop, { setter }]) => {
+    Object.entries(itemProps).forEach(([prop, { setter }]) => {
       setter(items[i][prop]);
     });
     setShowAddItem(!showAddItem);
   };
 
-  const handleDelete = i => () => {
+  const handleDelete = i => e => {
+    e.stopPropagation();
     setSelectedIndex(i);
     setShowDeleteWarning(true);
-    const it = window.confirm("Are you sure?");
-    if (it) {
-      setItems([...items.slice(0, i), undefined, ...items.slice(i + 1)]);
-    }
+  };
+
+  const handleConfirmDelete = () => {
+    setItems(updateElement(items)(selectedIndex)(() => undefined));
     setShowDeleteWarning(false);
+  };
+
+  const handleCancelDelete = () => {
+    setSelectedIndex(-1);
+    setShowDeleteWarning(false);
+  };
+
+  const cancelEdit = e => {
+    e.stopPropagation();
+    setShowEditOptions(false);
   };
 
   return (
@@ -86,20 +99,33 @@ const App = () => {
         {items.map((item, i) =>
           Boolean(item) ? (
             <ul
-              className="App__Item"
+              className={`App__Item ${
+                showEditOptions && selectedIndex === i ? "--editOptions" : ""
+              }`}
               key={item.upcId}
               onClick={handleEditClick(i)}
               role="button"
             >
               {showEditOptions && selectedIndex === i ? (
                 <>
-                  <li onClick={handleEdit(i)}>edit</li>
-                  <li onClick={handleDelete(i)}>delete</li>
+                  <li onClick={handleEdit(i)}>
+                    <i className="material-icons">edit</i>
+                  </li>
+                  <li onClick={handleDelete(i)}>
+                    <i className="material-icons" style={{ color: "red" }}>
+                      delete
+                    </i>
+                  </li>
+                  <li onClick={cancelEdit}>
+                    <i className="material-icons">close</i>
+                  </li>
                 </>
               ) : (
-                Object.keys(shoeProps).map(prop => (
+                Object.entries(itemProps).map(([prop, { label }]) => (
                   <li className={`--${prop}`} key={prop}>
-                    {item[prop]}
+                    <p>
+                      {label || capitalize(prop)}: {item[prop]}
+                    </p>
                   </li>
                 ))
               )}
@@ -110,42 +136,72 @@ const App = () => {
               className="App__Item --empty"
               role="button"
               onClick={handleAddClick(i)}
-            />
+            >
+              <i className="material-icons">add</i>
+            </ul>
           )
         )}
       </div>
 
       {showAddItem && (
         <div className="Modal" onClick={() => setShowAddItem(false)}>
-          <div className="Modal__close" onClick={() => setShowAddItem(false)}>
-            X
-          </div>
-          <form
-            className="App__AddItem"
-            onSubmit={addItem}
-            onClick={e => e.stopPropagation()}
-          >
-            {Object.entries(shoeProps).map(([prop, { val, setter }]) => {
-              return (
-                <input
-                  key={prop}
-                  type="text"
-                  placeholder={prop}
-                  value={val}
-                  onChange={targVal(setter)}
-                />
-              );
-            })}
+          <div className="Modal__inner">
+            <div
+              className="Modal__Header"
+              onClick={() => setShowAddItem(false)}
+            >
+              <h2>Shoe Information</h2>
+              <i className="material-icons Modal__Close">close</i>
+            </div>
+            <form
+              className="App__AddItem"
+              onSubmit={addItem}
+              onClick={e => e.stopPropagation()}
+            >
+              {Object.entries(itemProps).map(
+                ([prop, { val, setter, label }]) => {
+                  return (
+                    <input
+                      required
+                      key={prop}
+                      type="text"
+                      placeholder={label || capitalize(prop)}
+                      value={val}
+                      onChange={targVal(setter)}
+                    />
+                  );
+                }
+              )}
 
-            <button type="submit">
-              {itemIsSelected ? "Update" : "Add"} Shoe
-            </button>
-          </form>
+              <button
+                type="submit"
+                disabled={![brand, style, size, upcId].every(Boolean)}
+              >
+                {itemIsSelected ? "Update" : "Add"}
+              </button>
+            </form>
+          </div>
         </div>
       )}
       {showDeleteWarning && (
         <div className="Modal" onClick={() => setShowDeleteWarning(false)}>
-          Are you sure?
+          <div className="Modal__inner">
+            <div className="App__ConfirmDelete">
+              <p>Are you sure?</p>
+              <button
+                className="App__ConfirmDelete__Cancel"
+                onClick={handleCancelDelete}
+              >
+                Cancel
+              </button>
+              <button
+                className="App__ConfirmDelete__Confirm"
+                onClick={handleConfirmDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
